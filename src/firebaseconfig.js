75c65df2dd96f077598e38d5f591cbd136af05d0
rimshad1 <rimshadbr@gmail.com>
2025-03-27 05:05:43 +0530

    changes and deploye

---
+++ src/firebaseconfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBBwcYeV5hV3NIxutJEwMgtovCGVqKlMw",
+  authDomain: "luxlane-a3920.firebaseapp.com",
+  projectId: "luxlane-a3920",
+  storageBucket: "luxlane-a3920.firebasestorage.app",
+  messagingSenderId: "796839066660",
+  appId: "1:796839066660:web:b971f8ff99ba701c2a2cb1",
+  measurementId: "G-LF4E7S3VTD",
+};
+
+const app = initializeApp(firebaseConfig);
